9082dc57fcc3f8fa42ff39a4c609c0646cbed93c
TonySD <ahtoxa.b@gmail.com>
2024-03-24 04:10:07 +0300

    Impl: port scanner

---
+++ src/main/java/com/tonysd/nmap/PortScan.java
@@ -0,0 +1,64 @@
+package com.tonysd.nmap;
+
+import com.tonysd.entities.Machine;
+import com.tonysd.entities.Port;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PortScan {
+    private static final String machineScanCommand = "nmap -sC -sVVV %s";
+    private static final Pattern portInfoPattern = Pattern.compile("(\\d+)\\/(\\w+)\\s+(\\w+)\\s+(\\S+)\\s+(.*)", Pattern.MULTILINE);
+    public static List<Port> scanMachine(String ip) throws IOException, InterruptedException {
+        ProcessBuilder builder = new ProcessBuilder(machineScanCommand.formatted(ip).split(" "));
+        Process nmapScanner = builder.start();
+        nmapScanner.waitFor();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(nmapScanner.getInputStream()));
+
+        return parseNmapOutput(reader);
+    }
+
+    public static List<Port> parseNmapOutput(BufferedReader outputStream) throws IOException {
+        List<Port> ports = new ArrayList<>();
+        Port currentPort = null;
+        String outputLine, port, protocol, state, service, version;
+        String[] additionalInfo;
+        Matcher matcher;
+
+        while (!(outputLine = outputStream.readLine()).isBlank()) {
+            if (Character.isDigit(outputLine.charAt(0))) {
+                if (currentPort != null) ports.add(currentPort);
+                matcher = portInfoPattern.matcher(outputLine);
+                if (matcher.find()) {
+                    port = matcher.group(1);
+                    protocol = matcher.group(2);
+                    state = matcher.group(3);
+                    service = matcher.group(4);
+                    version = matcher.group(5);
+
+                    currentPort = new Port(
+                            Short.parseShort(port),
+                            protocol,
+                            state,
+                            service,
+                            version
+                    );
+                }
+            } else if (outputLine.charAt(0) == '|') {
+                additionalInfo = outputLine.substring(2).split(":\\s*", 2);
+                currentPort.getAdditional_info().put(
+                        additionalInfo[0],
+                        additionalInfo[1]
+                );
+            }
+        }
+
+        outputStream.close();
+        return ports;
+    }
+}
